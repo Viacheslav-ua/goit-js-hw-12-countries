096acc13d13802714a5ded6013b32b06931861af
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2021-08-12 02:09:04 +0300

    start parcel

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// import templateFunction from './templates/template.hbs';
+// const arrData = require('./menu.json');
+
+// // Рендеринг
+// const markup = arrData.map(item => templateFunction(item)).join('');
