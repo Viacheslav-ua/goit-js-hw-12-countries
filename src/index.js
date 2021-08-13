2d7e4b562046e930dc32bdcec3cf7e0c15b1680b
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2021-08-13 21:59:34 +0300

    2 стадия разработки

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
-// import templateFunction from './templates/template.hbs';
 import './css/styles.css'
 import fCountries from './js/fetchCountries'
+import listCountriesTpl from './templates/listCountries.hbs';
+import cardCountryTpl from './templates/cardCountry.hbs';
+// var debounce = require('lodash.debounce');
+import debounce from 'lodash.debounce';
 
 const fields = [
-    'name',
-    'capital',
-    'currencies',
+  'name',
+  'capital',
+  'population',
+  'languages',
+  'flag',
 ];
 
-fCountries('col', fields)
-.then(res => console.log(res))
+const refs = {
+  container: document.querySelector('.countries'),
+  searchFiled: document.querySelector('.search-filed'),
+}
 
+refs.searchFiled.addEventListener('input', debounce(onEnterName, 500));
 
-// // Рендеринг
-// const markup = arrData.map(item => templateFunction(item)).join('');
+function onEnterName(e) {
+  const strName = e.target.value;
+  console.log(strName)
+  fCountries(strName, fields)
+    .then(renderResult)
+    .catch(error => {
+       console.log('err', error);
+    });
+}
+
+function renderResult(result) {
+  if (result.length > 1 && result.length <= 10) {
+    const markup = listCountriesTpl(result.map(item => item.name));
+    refs.container.innerHTML = markup;
+    return
+  } else if (result.length === 1) {
+    const markup = cardCountryTpl(...result);
+    refs.container.innerHTML = markup;
+    return
+  } else if (result.length > 10){
+    refs.container.innerHTML = '';
+  } else {
+    refs.container.innerHTML = '';
+  }
+}
